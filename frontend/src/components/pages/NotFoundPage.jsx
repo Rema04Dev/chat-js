3697888b2e131316715e00b6bfcc7ba11e3fa846
Rema04Dev <romansmerdyakov@gmail.com>
2023-02-02 14:19:46 +0600

    realize NotFoundPage.jsx

--- frontend/src/components/pages/NotFoundPage.jsx
+++ frontend/src/components/pages/NotFoundPage.jsx
@@ -1,6 +1,11 @@
+import { Link } from 'react-router-dom';
 const NotFoundPage = () => {
     return (
-        <h1>NotFoundPage</h1>
+        <>
+            <h1>NotFoundPage</h1>
+            <Link to="/">Back to home</Link>
+        </>
+
     )
 }
 
