64529efbe95a216b2404fa15214d496c789df1da
Rema04Dev <romansmerdyakov@gmail.com>
2023-02-03 12:43:49 +0600

    add SignUpPage.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from "./pages/LoginPage";
+import SignUpPage from './pages/SignUpPage';
 import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
             <Routes>
                 <Route path='/' element={<LoginPage />} />
                 <Route path='/login' element={<LoginPage />} />
+                <Route path='/sign-up' element={<SignUpPage />} />
                 <Route path='*' element={<NotFoundPage />} />
             </Routes>
         </>
