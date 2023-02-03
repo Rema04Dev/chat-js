d564af81b4fa0bb76ddfeeb2f36c1cccaec921fa
Rema04Dev <romansmerdyakov@gmail.com>
2023-02-03 12:24:26 +0600

    update LoginPage.jsx

--- frontend/src/components/pages/LoginPage.jsx
+++ frontend/src/components/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import { Form, Button, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
@@ -16,30 +17,35 @@ const LoginPage = () => {
             password: Yup.string().min(4, 'Пароль должен содержать минимум 4 символа').required('Обязательное поле')
         })
     })
+
     return (
         <>
             <Container>
-                <Form onSubmit={formik.handleSubmit}>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Ваш логин</Form.Label>
+                <h1 className='text-center'> Войти</h1>
+                <Form
+                    onSubmit={formik.handleSubmit}>
+                    <Form.Group className="mb-3 form-floating">
                         <Form.Control
                             value={formik.values.login}
                             onChange={formik.handleChange}
                             type="text"
+                            id="floatingLogin"
                             name="login"
                             placeholder="Введите логин" />
+                        <Form.Label htmlFor='floatingLogin'>Ваш логин</Form.Label>
                         <Form.Text className="text-danger">
                             {formik.errors.login && formik.touched.login ? formik.errors.login : null}
                         </Form.Text>
                     </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Ваш пароль</Form.Label>
+                    <Form.Group className="mb-3 form-floating">
                         <Form.Control
                             value={formik.values.password}
                             onChange={formik.handleChange}
                             type="password"
+                            id="floatingPassword"
                             name="password"
                             placeholder="Введите пароль" />
+                        <Form.Label htmlFor='floatingPassword'>Ваш пароль</Form.Label>
                         <Form.Text className="text-danger">
                             {formik.errors.password && formik.touched.password ? formik.errors.password : null}
                         </Form.Text>
@@ -48,6 +54,7 @@ const LoginPage = () => {
                         Войти
                     </Button>
                 </Form>
+                <p>Нет аккаунта? <Link to="/sign-up">Регистрация</Link></p>
             </Container>
 
         </>
