dbecf2f9d78d92a124a0c90b52e3aaad3dc2ebb5
Rema04Dev <romansmerdyakov@gmail.com>
2023-02-04 11:28:27 +0600

    realize private routing & auth

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,15 +1,32 @@
+import { useState, useMemo } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
-
+import AuthContext from '../contexts/AuthContext';
+import PrivateRoute from './PrivateRoute'
+import { useCallback } from 'react';
 const App = () => {
+    const [user, setUser] = useState(null);
+    // const logIn = (userData, token) => {
+    //     localStorage.setItem('user', { ...userData, token });
+    //     setUser(true);
+    // }
+    const logIn = useCallback((user) => {
+        localStorage.setItem('user', JSON.stringify(user));
+        setUser(true);
+    })
     return (
         <>
-            <Routes>
-                <Route path='/' element={<LoginPage />} />
-                <Route path='/login' element={<LoginPage />} />
-                <Route path='*' element={<NotFoundPage />} />
-            </Routes>
+            <AuthContext.Provider value={{ user, logIn }}>
+                <Routes>
+                    <Route element={<PrivateRoute />}>
+                        <Route path='/' element={<MainPage />} />
+                    </Route>
+                    <Route path='/login' element={<LoginPage />} />
+                    <Route path='*' element={<NotFoundPage />} />
+                </Routes>
+            </AuthContext.Provider>
         </>
     );
 };
--- frontend/src/components/pages/LoginPage.jsx
+++ frontend/src/components/pages/LoginPage.jsx
@@ -1,20 +1,52 @@
+import { useState } from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useFormik } from 'formik';
+import { useContext } from 'react';
+import AuthContext from '../../contexts/AuthContext';
 import * as Yup from 'yup';
+import axios from 'axios';
+import routes from '../../utils/routes'
 
 const LoginPage = () => {
+    const [errorMessage, setErrorMessage] = useState(null);
+    const navigate = useNavigate();
+    const { logIn } = useContext(AuthContext);
+
     const formik = useFormik({
         initialValues: {
-            login: '',
+            username: '',
             password: '',
         },
-        onSubmit: (values) => {
-            console.log(JSON.stringify(values, null, 2));
+        onSubmit: async (values) => {
+            const userData = {
+                username: values.username,
+                password: values.password
+            }
+            try {
+                const response = await axios.post(routes.loginPath(), { ...userData })
+                if (response.statusText !== 'OK') {
+                    const status = await response.data.error;
+                    throw new Error(status);
+                }
+                const token = await response.data;
+                logIn({ ...userData, token })
+                navigate('/');
+            } catch (e) {
+                setErrorMessage(e.response.statusText)
+                throw e;
+            }
+
         },
         validationSchema: Yup.object({
-            login: Yup.string().min(3, 'Логин должен содержать минимум 3 символа').required('Обязательное поле'),
-            password: Yup.string().min(4, 'Пароль должен содержать минимум 4 символа').required('Обязательное поле')
+            username: Yup
+                .string()
+                .min(3, 'Логин должен содержать минимум 3 символа')
+                .required('Обязательное поле'),
+            password: Yup
+                .string()
+                .min(4, 'Пароль должен содержать минимум 4 символа')
+                .required('Обязательное поле')
         })
     })
 
@@ -26,15 +58,15 @@ const LoginPage = () => {
                     onSubmit={formik.handleSubmit}>
                     <Form.Group className="mb-3 form-floating">
                         <Form.Control
-                            value={formik.values.login}
+                            value={formik.values.username}
                             onChange={formik.handleChange}
                             type="text"
                             id="floatingLogin"
-                            name="login"
+                            name="username"
                             placeholder="Введите логин" />
                         <Form.Label htmlFor='floatingLogin'>Ваш логин</Form.Label>
                         <Form.Text className="text-danger">
-                            {formik.errors.login && formik.touched.login ? formik.errors.login : null}
+                            {formik.errors.username && formik.touched.username ? formik.errors.username : null}
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3 form-floating">
@@ -48,6 +80,9 @@ const LoginPage = () => {
                         <Form.Label htmlFor='floatingPassword'>Ваш пароль</Form.Label>
                         <Form.Text className="text-danger">
                             {formik.errors.password && formik.touched.password ? formik.errors.password : null}
+                        </Form.Text>
+                        <Form.Text className="text-danger">
+                            {errorMessage}
                         </Form.Text>
                     </Form.Group>
                     <Button variant="primary" type="submit">
