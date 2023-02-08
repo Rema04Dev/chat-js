bfd12e17c284987a31fc04ca17ba7704de0a78b3
Rema04Dev <romansmerdyakov@gmail.com>
2023-02-08 13:10:18 +0600

    add dataPath to routes.js

--- frontend/src/utils/routes.js
+++ frontend/src/utils/routes.js
@@ -1,6 +1,7 @@
 const apiPath = 'api/v1';
 const routes = {
-    loginPath: () => [apiPath, 'login'].join('/')
+    loginPath: () => [apiPath, 'login'].join('/'),
+    dataPath: () => [apiPath, 'data'].join('/')
 };
 
 export default routes;
