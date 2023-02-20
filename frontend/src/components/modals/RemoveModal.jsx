7fa9c4109819560e634fa9470982ca2bb19d384d
Rema04Dev <romansmerdyakov@gmail.com>
2023-02-20 14:19:42 +0600

    update RemoveModal

--- frontend/src/components/modals/RemoveModal.jsx
+++ frontend/src/components/modals/RemoveModal.jsx
@@ -1,15 +1,15 @@
-import { Button, Modal } from 'react-bootstrap';
-import notification from '../../utils/notify';
-import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
+import { Button, Modal } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 import { hideModal } from '../../store/slices/modalsSlice';
 import useSocket from '../../hooks/useSocket.hook';
+import notification from '../../utils/notify';
 
 const RemoveModal = () => {
-    const { t } = useTranslation();
     const dispatch = useDispatch();
-    const { removeChannel } = useSocket();
     const channelId = useSelector(state => state.modals.channelId);
+    const { removeChannel } = useSocket();
+    const { t } = useTranslation();
 
     const handleRemove = () => {
         removeChannel(channelId);
@@ -29,12 +29,14 @@ const RemoveModal = () => {
                         <Button
                             className='m-1'
                             variant="secondary"
+                            role="cancel"
                             onClick={() => dispatch(hideModal())}>
                             {t('removeModal.cancel')}
                         </Button>
                         <Button
                             className='m-1'
                             variant="danger"
+                            role="confirm"
                             onClick={handleRemove}>
                             {t('removeModal.remove')}
                         </Button>
