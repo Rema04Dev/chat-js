c7e2432ed9498e03485ed5e7544b3f3656dc84e2
Roman <romansmerdyakov@gmail.com>
2023-03-17 14:52:29 +0600

    clean off

--- frontend/src/components/skeletons/CustomSpinner.jsx
+++ frontend/src/components/skeletons/CustomSpinner.jsx
@@ -1,16 +1,13 @@
 import { Spinner } from 'react-bootstrap';
 
 const CustomSpinner = ({ size }) => (
-  <>
-    <Spinner
-      as="span"
-      animation="border"
-      size={size}
-      role="status"
-      aria-hidden="true"
-    />
-    <span className="visually-hidden">Loading...</span>
-  </>
+  <Spinner
+    as="span"
+    animation="border"
+    size={size}
+    role="status"
+    aria-hidden="true"
+  />
 );
 
 export default CustomSpinner;
--- frontend/src/utils/promisify.js
+++
@@ -1,8 +0,0 @@
-export default (fn) => (...args) => new Promise(
-  (resolve, reject) => {
-    fn(...args, (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    });
-  },
-);
