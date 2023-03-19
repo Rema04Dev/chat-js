5e372d5498183e15831510cff7ade42c9319c6cb
Roman <romansmerdyakov@gmail.com>
2023-03-19 14:54:12 +0600

    fix socketApi

--- frontend/src/components/SocketProvider.jsx
+++ frontend/src/components/SocketProvider.jsx
@@ -1,61 +1,45 @@
-import { useCallback, useEffect, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
 import SocketContext from '../contexts/SocketContext';
 import * as messagesActions from '../store/slices/messagesSlice';
 import * as channelsActions from '../store/slices/channelsSlice';
 
+const promisifySocket = (socket, event, data) => new Promise((resolve, reject) => {
+  socket.timeout(5000).emit(event, data, (err, response) => {
+    if (err) {
+      reject(err);
+    }
+    if (response?.status === 'ok') {
+      resolve(response.data);
+    }
+  });
+});
+
 const SocketProvider = ({ children, socket }) => {
   const dispatch = useDispatch();
 
-  const promisifySocket = useCallback((event, data) => new Promise((resolve, reject) => {
-    socket.timeout(5000).emit(event, data, (err, response) => {
-      if (err) {
-        reject(err);
-      }
-      if (response?.status === 'ok') {
-        resolve(response.data);
-      }
-    });
+  const socketApi = useMemo(() => ({
+    addMessage: (data) => promisifySocket(socket, 'newMessage', data),
+    addChannel: (data) => promisifySocket(socket, 'newChannel', data),
+    renameChannel: (data) => promisifySocket(socket, 'renameChannel', data),
+    removeChannel: (data) => promisifySocket(socket, 'removeChannel', data),
   }), [socket]);
 
-  const socketApi = useMemo(() => ({
-    addMessage: (data) => promisifySocket('newMessage', data),
-    addChannel: (data) => promisifySocket('newChannel', data),
-    renameChannel: (data) => promisifySocket('renameChannel', data),
-    removeChannel: (data) => promisifySocket('removeChannel', data),
-  }), [promisifySocket]);
-
-  useEffect(() => {
-    const handleNewMessage = (message) => {
-      dispatch(messagesActions.addMessage(message));
-    };
-    const handleNewChannel = (channel) => {
-      dispatch(channelsActions.addChannel(channel));
-    };
-    const handleRenameChannel = (channelName) => {
-      dispatch(channelsActions.renameChannel(channelName));
-    };
-    const handleRemoveChannel = (channelId) => {
-      dispatch(channelsActions.removeChannel(channelId));
-    };
-
-    socket.on('newMessage', handleNewMessage);
-    socket.on('newChannel', handleNewChannel);
-    socket.on('renameChannel', handleRenameChannel);
-    socket.on('removeChannel', handleRemoveChannel);
-
-    return () => {
-      socket.off('newMessage', handleNewMessage);
-      socket.off('newChannel', handleNewChannel);
-      socket.off('renameChannel', handleRenameChannel);
-      socket.off('removeChannel', handleRemoveChannel);
-    };
-  }, [socket, dispatch]);
-
-  const socketValue = useMemo(() => ({ socketApi }), [socketApi]);
+  socket.on('newMessage', (message) => {
+    dispatch(messagesActions.addMessage(message));
+  });
+  socket.on('newChannel', (channel) => {
+    dispatch(channelsActions.addChannel(channel));
+  });
+  socket.on('renameChannel', (channelName) => {
+    dispatch(channelsActions.renameChannel(channelName));
+  });
+  socket.on('removeChannel', (channelId) => {
+    dispatch(channelsActions.removeChannel(channelId));
+  });
 
   return (
-    <SocketContext.Provider value={socketValue}>
+    <SocketContext.Provider value={socketApi}>
       {children}
     </SocketContext.Provider>
   );
